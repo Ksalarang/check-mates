6aeb9a4b7b4ec084a55c8066dd3d9cab5e839f52
ksalarang <anbleidd@gmail.com>
2024-03-18 22:17:59 +0500

    add king movements

--- Assets/Scripts/GameScene/Models/Pieces/King.cs
+++ Assets/Scripts/GameScene/Models/Pieces/King.cs
@@ -3,8 +3,24 @@ using GameScene.Models.BoardModel;
 
 namespace GameScene.Models.Pieces {
 public class King : Piece {
+    List<PieceDirection> directions;
+
+    protected override void awake() {
+        directions = PieceDirectionExt.getAllDirections();
+    }
+
     public override List<Square> getAvailableSquares() {
         var list = new List<Square>();
+        foreach (var direction in directions) {
+            var square = getSquareInDirection(direction);
+            if (square is null) continue;
+            
+            if (!square.hasPiece()) {
+                list.Add(square);
+            } else if (!isSameSide(square.currentPiece) && square.currentPiece.type != PieceType.King) {
+                list.Add(square);
+            }
+        }
         return list;
     }
 }
